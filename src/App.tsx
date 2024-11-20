74efca50f2a1142db6170e5f7f8d275f10e453bf
Anderson Pontes <andersonpinheiro.developer@gmail.com>
2024-11-20 00:15:19 -0300

    configuração inicial do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-
+import { Button } from '@/components/ui/button'
+import './index.css'
 
 export function App() {
 
   return (
     <>
-      <h1>Portal Arara-Azul</h1>
+    <div className='flex justify-center items-center h-screen'>
+        <Button>Enviar</Button>
+
+    </div>
+
     </>
   )
 }
