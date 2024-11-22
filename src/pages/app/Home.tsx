74aa9be58ae8086b695eb1fe676cebfa23162fe2
Anderson Pontes <andersonpinheiro.developer@gmail.com>
2024-11-21 23:18:53 -0300

    ajuste no flip dos cards

--- src/pages/app/Home.tsx
+++ src/pages/app/Home.tsx
@@ -1,12 +1,12 @@
 import { Helmet } from "react-helmet-async";
-import imgMaps from '@/assets/maps.png';
-import imgAcordo from '@/assets/acordo.png';
-import imgSaneamento from '@/assets/saneamento.png';
-import imgIndicadores from '@/assets/indicadores.png';
-import imgEngenharia from '@/assets/engenharia.png';
-import imgCamara from '@/assets/camara.png';
-import imgDespesa from '@/assets/despesa.png';
-import { Link } from 'react-router-dom';
+import imgMaps from "@/assets/maps.png";
+import imgAcordo from "@/assets/acordo.png";
+import imgSaneamento from "@/assets/saneamento.png";
+import imgIndicadores from "@/assets/indicadores.png";
+import imgEngenharia from "@/assets/engenharia.png";
+import imgCamara from "@/assets/camara.png";
+import imgDespesa from "@/assets/despesa.png";
+import { Link } from "react-router-dom";
 
 export function Home() {
     const flashcards = [
@@ -44,18 +44,18 @@ export function Home() {
             title: "AGREGADOR DE TABELAS DE CUSTOS DE ENGENHARIA",
             description:
                 "Dados das tabelas de custos de insumos e composições de serviços de obras e serviços de engenharia",
-            image: imgEngenharia
+            image: imgEngenharia,
         },
         {
             title: "CÂMARAS EM FOCO",
             description: "Panorama financeiro das câmaras municipais paraenses",
-            image: imgCamara
+            image: imgCamara,
         },
         {
             title: "PANORAMA MUNICIPAL - DESPESAS",
             description:
                 "Panorama das despesas dos municípios paraenses",
-            image: imgDespesa
+            image: imgDespesa,
         },
     ];
 
@@ -70,11 +70,11 @@ export function Home() {
                     {flashcards.map((card, index) => (
                         <div
                             key={index}
-                            className="group perspective relative w-full h-64 rounded-3xl shadow-md overflow-hidden transform transition-transform duration-700"
+                            className="group perspective-1000 relative w-full h-64 rounded-3xl shadow-md overflow-hidden"
                         >
                             {/* Container 3D */}
-                            <div className="relative w-full h-full transform-style-3d group-hover:rotate-y-180">
-                                {/* Frente do Card (Imagem + Título + Parágrafo) */}
+                            <div className="relative w-full h-full transform-style-3d transition-transform duration-700 ease-in-out group-hover:rotate-y-180">
+                                {/* Frente do Card */}
                                 <div className="absolute inset-0 bg-white rounded-3xl flex flex-col items-center justify-center text-center shadow-lg backface-hidden p-6">
                                     <img
                                         src={card.image}
@@ -87,20 +87,14 @@ export function Home() {
                                     <p className="text-sm text-slate-600">{card.description}</p>
                                 </div>
 
-                                {/* Verso do Card (Mensagem Oculta) */}
+                                {/* Verso do Card */}
                                 <div className="absolute inset-0 bg-gradient-to-r from-cyan-500 to-blue-500 rounded-3xl flex items-center justify-center text-center rotate-y-180 backface-hidden p-6 text-white">
                                     <Link
                                         to={card.route || "/"}
-                                        className="text-lg font-semibold underline hover:text-blue-300">
-
-                                        <h1 className="text-lg font-semibold">
-                                            {card.title}.
-                                        </h1>
-
-
-
+                                        className="text-lg font-semibold underline hover:text-blue-300"
+                                    >
+                                        <h1 className="text-lg font-semibold">{card.title}</h1>
                                     </Link>
-
                                 </div>
                             </div>
                         </div>
