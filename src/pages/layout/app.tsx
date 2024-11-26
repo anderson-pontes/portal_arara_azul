08bc0d52a1b3f5e388a76abfb7b4a5ca2291d759
Anderson Pontes <andersonpinheiro.developer@gmail.com>
2024-11-26 16:04:47 -0300

    ajuste no footer

--- src/pages/layout/app.tsx
+++ src/pages/layout/app.tsx
@@ -6,14 +6,20 @@ export function AppLayout() {
   return (
     <div className="flex min-h-screen flex-col antialiased">
 
-        <Header />
+        <div className="sticky top-0 z-50">
+            
+            <Header />
+
+        </div>
+
+        
 
       <div className="flex flex-1 flex-col gap-4 p-8 pt-6 bg-blue-100">
 
         <Outlet />
 
         <footer className="text-sm text-muted-foreground text-center mt-8">
-          Copyright &copy; {new Date().getFullYear()} | MULT TECH CONSULTORIA E TECNOLOGIA LTDA | Todos os direitos reservados. 
+          Copyright &copy; {new Date().getFullYear()} | Todos os direitos reservados. 
         </footer>
 
       </div>
