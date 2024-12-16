7eb66a3cbd98342662bb6cfc21829f23b862bb26
Anderson Pontes <andersonpinheiro.developer@gmail.com>
2024-12-16 14:47:22 -0300

    alteração do input date para controller

--- src/pages/app/SearchFilter.tsx
+++ src/pages/app/SearchFilter.tsx
@@ -4,11 +4,12 @@ import { useNavigate } from 'react-router-dom';
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Search, X } from 'lucide-react';
-
+import { useForm, Controller } from 'react-hook-form';
+import { DatePicker } from '@/pages/app/Date';
 
 const SearchFilter: React.FC = () => {
     const { query, setQuery } = useContext(SearchContext)!;
-    const [localQuery, setLocalQuery] = useState<{ trecho: string; nome_arquivo: string; link_arquivo: string; termo: string; ano: string; data_inicio: string; data_fim: string; }>({
+    const [localQuery, setLocalQuery] = useState({
         trecho: query.trecho || '',
         nome_arquivo: query.nome_arquivo || '',
         link_arquivo: query.link_arquivo || '',
@@ -16,8 +17,9 @@ const SearchFilter: React.FC = () => {
         ano: query.ano || '',
         data_inicio: query.data_inicio || '',
         data_fim: query.data_fim || '',
-
     });
+
+    const { control, setValue } = useForm(); // Adiciona setValue do useForm
     const navigate = useNavigate();
 
     const handleSearch = (event: React.FormEvent) => {
@@ -28,7 +30,7 @@ const SearchFilter: React.FC = () => {
 
     const handleClearFilters = () => {
         // Reseta o estado local para os valores iniciais
-        setLocalQuery({
+        const initialValues = {
             trecho: '',
             nome_arquivo: '',
             link_arquivo: '',
@@ -36,23 +38,19 @@ const SearchFilter: React.FC = () => {
             ano: '',
             data_inicio: '',
             data_fim: '',
-        });
+        };
+        setLocalQuery(initialValues);
 
-        // Também reseta o contexto se necessário
-        setQuery({
-            trecho: '',
-            nome_arquivo: '',
-            link_arquivo: '',
-            termo: '',
-            ano: '',
-            data_inicio: '',
-            data_fim: '',
-        });
+        
+        setQuery(initialValues);
+
+        
+        setValue('data_inicio', ''); 
+        setValue('data_fim', '');    
     };
 
     return (
         <form className="flex flex-col sm:flex-row items-center gap-2 flex-wrap">
-
             <span className="text-lg font-semibold text-blue-800/80">Pesquisar:</span>
             <Input
                 placeholder="Informe o conteúdo a ser buscado"
@@ -66,35 +64,29 @@ const SearchFilter: React.FC = () => {
                 onChange={(e) => setLocalQuery({ ...localQuery, ano: e.target.value })}
                 className="w-full sm:w-auto"
             />
-            <Input
-                placeholder="Data Inicial - dd/mm/aaaa"
-                value={localQuery.data_inicio}
-                onChange={(e) => setLocalQuery({ ...localQuery, data_inicio: e.target.value })}
-                className="w-full sm:w-auto"
+            <Controller
+                name="data_inicio"
+                control={control}
+                render={({ field }) => (
+                    <DatePicker date={field.value} onChange={(date) => field.onChange(date)} />
+                )}
             />
-            <Input
-                placeholder="Data fim - dd/mm/aaaa"
-                value={localQuery.data_fim}
-                onChange={(e) => setLocalQuery({ ...localQuery, data_fim: e.target.value })}
-                className="w-full sm:w-auto"
+            <Controller
+                name="data_fim"
+                control={control}
+                render={({ field }) => (
+                    <DatePicker date={field.value} onChange={(date) => field.onChange(date)} />
+                )}
             />
-
-
             <Button onClick={handleSearch} type="submit" size="default" className='bg-gradient-to-r from-blue-400 to-indigo-600 shadow-lg text-white hover:text-blue-300'>
                 <Search className="h-4 w-4 mr-2" />
                 Pesquisar
             </Button>
-
             <Button onClick={handleClearFilters} variant="outline" size="default" className="w-full sm:w-auto">
                 <X className="h-4 w-4 mr-2" />
                 Remover filtros
             </Button>
-
-
-
         </form>
-
-
     );
 };
 
